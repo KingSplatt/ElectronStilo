b17de44e4c7cdd2251a7ce524779a9d49acc2e94
KingSplatt <malp18774@gmail.com>
2024-12-22 23:58:18 -0700

    Hola

---
+++ frontend/src/modulos/principal/principalVue.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>Principal</h1>
+        <p>Esto es la página principal</p>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>

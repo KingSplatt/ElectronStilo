9ebe05ea9ef860f2a2beb2d049328c819941e0cb
KingSplatt <malp18774@gmail.com>
2025-01-05 00:16:31 -0700

    hola

--- frontend/src/modulos/pruebas/mainVue.vue
+++ frontend/src/modulos/pruebas/mainVue.vue
@@ -1,7 +1,21 @@
 <template>
     <div>
-        <h1>Pruebas</h1>
-        <p>Esto es una prueba</p>
+        <nav>
+            <ul id="barra">
+                <li>
+                    inicio
+                </li>
+                <li>
+                    productos
+                </li>
+                <li>
+                    salir
+                </li>
+            </ul>
+            <h1>Pruebas</h1>
+            <p>Esto es una prueba</p>
+
+        </nav>
     </div>
 </template>
 
@@ -10,5 +24,27 @@
 </script>
 
 <style scoped>
-
+#barra {
+    list-style: none;
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: space-around;
+}
+li{
+    margin: 10px;
+}
+div{
+    width: 100%;
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: space-between;
+    background-color: #333;
+    color: white;
+    list-style: none;
+    padding: 10px;
+    margin: 0;
+}
+nav{
+    width: 100%;
+}
 </style>
